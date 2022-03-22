3fc1b72a0529a78cfeb9ae9487941d5f6628bf17
ahmedraza16 <101126286+ahmedraza16@users.noreply.github.com>
2022-03-22 19:29:53 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+
+$("#btn-4").click(function(){
+  $("#skil").toggle();
+});
+
+
+$("#btn-2").click(function(){
+  $("#about").toggle();
+});
+
+$("#btn-3").click(function(){
+  $("#educate").toggle();
+});
