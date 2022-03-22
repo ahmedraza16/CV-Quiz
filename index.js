f32d73ec4c25cd3467b48d838723647e84348a80
ahmedraza16 <101126286+ahmedraza16@users.noreply.github.com>
2022-03-22 19:35:27 +0500

    Commit

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 
-$("#btn-4").click(function(){
-  $("#skil").toggle();
-});
+$( document ).ready(function() {
+  
+  alert("The document is loaded. Click on the icons to view the information")
+  $("#btn-4").click(function(){
+    $("#skil").toggle();
+  });
+  
+  
+  $("#btn-2").click(function(){
+    $("#about").toggle();
+  });
+  
+  $("#btn-3").click(function(){
+    $("#educate").toggle();
+  });
 
 
-$("#btn-2").click(function(){
-  $("#about").toggle();
-});
-
-$("#btn-3").click(function(){
-  $("#educate").toggle();
 });
